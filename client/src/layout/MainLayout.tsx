ff449a667daee2a6fa54a20af4d1c89b35edc020
paresiqbal <thecrowdel@gmail.com>
2024-01-19 23:36:28 +0700

    Add Layout

---
+++ client/src/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+// rdd
+import { Outlet } from "react-router";
+
+export default function MainLayout() {
+  return (
+    <div>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
