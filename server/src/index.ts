2c065328e0db45f1fcbfac1c71aba832e0318b23
paresiqbal <thecrowdel@gmail.com>
2024-01-19 16:53:48 +0700

    First Commit

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+// library
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+// app
+const app = express();
+
+// middleware
+app.listen(3001, () => console.log("server is running"));
