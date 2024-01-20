a889b11543196a1a435d9e8d5856a93f09a937a1
paresiqbal <thecrowdel@gmail.com>
2024-01-20 18:54:11 +0700

    Test

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <h1>E-Commerce</h1>
       <NavLink to="/"></NavLink>
     </nav>
   );
