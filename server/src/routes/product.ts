3d9e95009e143df7ff806b342f6a1c9e02df94ff
paresiqbal <thecrowdel@gmail.com>
2024-01-24 23:19:50 +0700

    asdasd

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -3,16 +3,35 @@ import { Router, Request, Response } from "express";
 
 // model
 import { ProductModel } from "../models/product";
+import { UserModel } from "../models/User";
+
+// route
+import { verifyToken } from "./user";
 
 const router = Router();
 
-router.get("/", async (_, res: Response) => {
+router.get("/", verifyToken, async (_, res: Response) => {
   // get all product
-
   try {
     const products = await ProductModel.find({});
 
     res.json({ products });
+  } catch (error) {
+    res.status(400).json({ error: "Ups something wrong" });
+  }
+});
+
+router.post("/checkout", verifyToken, async (req: Request, res: Response) => {
+  const { customerID, cartItems } = req.body;
+
+  try {
+    const user = await UserModel.findById(customerID);
+    const productIDs = Object.keys(cartItems);
+    const products = await ProductModel.find({ _id: { $in: productIDs } });
+
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
   } catch (error) {
     res.status(400).json({ error: "Ups something wrong" });
   }
