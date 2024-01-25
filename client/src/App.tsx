469754ab1a045c7b63507366a11060021dd7afe0
paresiqbal <thecrowdel@gmail.com>
2024-01-25 22:18:49 +0700

    Route complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
 // rdd
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// layout
 import MainLayout from "./layout/MainLayout";
+import ErrorLayout from "./layout/ErrorLayout";
+
+// pages
+import Shop from "@/pages/shop";
 
 // route
-const router = createBrowserRouter([{ path: "/", element: <MainLayout /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Shop />,
+        errorElement: <ErrorLayout />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
---
+++ client/src/layout/ErrorLayout.tsx
@@ -0,0 +1,7 @@
+export default function ErrorLayout() {
+  return (
+    <div className="text-center font-bold text-4xl">
+      <h1>Ups. Something Wrong</h1>
+    </div>
+  );
+}
