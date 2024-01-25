cb0391973f64f589a0a781dc928a1434d446dac7
paresiqbal <thecrowdel@gmail.com>
2024-01-25 21:18:13 +0700

    Login route complete middleware for verify token

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 // library
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // models
-import { UserModel } from "../models/User";
+import { IUser, UserModel } from "../models/User";
 
 // error handling
 import { UserErrors } from "../error";
@@ -31,5 +32,48 @@ router.post("/register", async (req: Request, res: Response) => {
     res.status(500).json({ type: error });
   }
 });
+
+// route to login account
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    // search user in collection
+    const user: IUser = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    // compare hash password in collection with user input password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ type: UserErrors.WRONG_CREDENTIAL });
+    }
+
+    const token = jwt.sign({ id: user._id }, "secrete");
+    res.json({ token, userID: user._id });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secrete", (err, user) => {
+      if (err) {
+        return res.status(403);
+      }
+
+      next();
+    });
+  }
+
+  return res.sendStatus(401);
+};
 
 export { router as userRouter };
