5868aa2c1be0d670002f4a2283b9a0416db72b88
paresiqbal <thecrowdel@gmail.com>
2024-01-27 23:40:41 +0700

    Just push

--- client/src/pages/shop/index.tsx
+++ client/src/pages/shop/index.tsx
@@ -2,6 +2,14 @@ export default function index() {
   return (
     <div>
       <h1>Shop</h1>
+      <div>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          quaerat debitis quod, inventore praesentium quia numquam placeat nihil
+          impedit tenetur?
+        </p>
+      </div>
     </div>
   );
 }
