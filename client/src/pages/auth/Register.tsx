50067d8fdae8e6cd389a37326c2cf48ae7beadef
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-01-29 22:46:56 +0700

    Register UI complete

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -1,4 +1,12 @@
+// zod
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+// shadcn
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
@@ -7,32 +15,101 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+// icons
+import { FaFacebook } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+
+// schema
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
 
 export function Register() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your email below to create your account.
+        </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-            </div>
-          </div>
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="pares" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </form>
+        </Form>
       </CardContent>
+      <div className="flex items-center my-4">
+        <div className="flex-grow">
+          <Separator className="my-4" />
+        </div>
+        <span className="mx-4 text-sm text-gray-600">OR CONTINUE WITH</span>
+        <div className="flex-grow">
+          <Separator className="my-4" />
+        </div>
+      </div>
       <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+        <Button className="flex gap-2 items-center">
+          <FaGoogle /> Google
+        </Button>
+        <Button className="flex gap-2 items-center">
+          <FaFacebook /> Facebook
+        </Button>
       </CardFooter>
     </Card>
   );
