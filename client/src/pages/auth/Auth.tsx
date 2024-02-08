64949c782bd118e3beecc3138df3650f96630916
Pahreza Iqbal Prastowo <73816062+paresiqbal@users.noreply.github.com>
2024-02-08 20:00:07 +0700

    asdasd

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -7,7 +7,7 @@ export default function Auth() {
     <div className="py-10">
       <h1 className="text-9xl font-bold">Let's find what you need here</h1>
       <p className="text-4xl mt-4">
-        The best procurement store, that you can find in Curup.
+        The best procurement store, that you can find in your country.
       </p>
       <div className="flex w-full max-w-sm pt-10 items-center space-x-2">
         <Input type="email" placeholder="Email" />
